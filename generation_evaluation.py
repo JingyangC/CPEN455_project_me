c3d9247924abaebaac51d952997183d18b0a7a27
JingyangC <143899811+JingyangC@users.noreply.github.com>
2025-04-04 17:06:15 -0700

    generation_evaluation.py can run now for OG

--- generation_evaluation.py
+++ generation_evaluation.py
@@ -21,6 +21,8 @@ import argparse
 # This is a demonstration of how to call the sample function, feel free to modify it
 # You should modify this sample function to get the generated images from your model
 # You should save the generated images to the gen_data_dir, which is fixed as 'samples'
+"""
+# OG
 sample_op = lambda x : sample_from_discretized_mix_logistic(x, 5)
 def my_sample(model, gen_data_dir, sample_batch_size = 25, obs = (3,32,32), sample_op = sample_op):
     for label in my_bidict:
@@ -30,12 +32,37 @@ def my_sample(model, gen_data_dir, sample_batch_size = 25, obs = (3,32,32), samp
         sample_t = rescaling_inv(sample_t)
         save_images(sample_t, os.path.join(gen_data_dir), label=label)
     pass
+#"""
+#"""
+# OG
+sample_op = lambda x : sample_from_discretized_mix_logistic(x, args.nr_logistic_mix)
+def my_sample(model, gen_data_dir, sample_batch_size = 10, obs = (3,32,32), sample_op = sample_op):
+    for label in my_bidict:
+        print(f"Label: {label}")
+        #generate images for each label, each label has 10 images
+        sample_t = sample(model, sample_batch_size, obs, sample_op)
+        sample_t = rescaling_inv(sample_t)
+        save_images(sample_t, os.path.join(gen_data_dir), label=label)
+    pass
+#"""
 # End of your code
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--ref_data_dir', type=str,
                         default="data/test", help='Location for the dataset')
+    
+    #"""
+    #added for convenient
+    parser.add_argument('-l', '--load_params', type=str, default=None,
+                        help='Restore training from previous model checkpoint?')
+    parser.add_argument('-q', '--nr_resnet', type=int, default=1,
+                        help='Number of residual blocks per stage of the model')
+    parser.add_argument('-n', '--nr_filters', type=int, default=40,
+                        help='Number of filters to use across the model. Higher = larger model.')
+    parser.add_argument('-m', '--nr_logistic_mix', type=int, default=5,
+                        help='Number of logistic components in the mixture. Higher = more flexible model')
+    #"""
     
     args = parser.parse_args()
     
@@ -44,14 +71,31 @@ if __name__ == "__main__":
     BATCH_SIZE=128
     device = "cuda" if torch.cuda.is_available() else "cpu"
     
+    device = "mps"      #add for mps
+    
     if not os.path.exists(gen_data_dir):
         os.makedirs(gen_data_dir)
 
     #TODO: Begin of your code
     #Load your model and generate images in the gen_data_dir, feel free to modify the model
+    """
+    #OG
     model = PixelCNN(nr_resnet=1, nr_filters=40, input_channels=3, nr_logistic_mix=5)
     model = model.to(device)
     model = model.eval()
+    #"""
+    
+    #"""
+    #new 
+    model = PixelCNN(nr_resnet=args.nr_resnet, nr_filters=args.nr_filters, 
+                input_channels=3, nr_logistic_mix=args.nr_logistic_mix)
+    model = model.to(device)
+    model = model.eval()
+    model.device = "mps"        #added for mps, need same for cuda?
+
+    if args.load_params:
+        model.load_state_dict(torch.load(args.load_params))
+        print('model parameters loaded')
     #End of your code
     
     my_sample(model=model, gen_data_dir=gen_data_dir)
